737905becc4defc916cb40485af518d984c2d99e
afrolovskiy <frolovskiyav@gmail.com>
2013-10-09 00:13:14 +0400

    made corrections

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -5,6 +5,10 @@ global x_left; x_left = -1.0;
 global x_right; x_right = 1.0;
 global sigma; sigma = 2.0;
 
-draw_results(@point_function, -3.0, 3.0, 1.0);
-draw_results(@rect_function, -3, 3, 1.0);
-draw_results(@gauss_function, -3, 3, 1.0);
+a = -3.0;
+b = 3.0;
+dx = 0.1;
+
+draw_results(@point_function, a, b, dx);
+draw_results(@rect_function, a, b, dx);
+draw_results(@gauss_function, a, b, dx);
