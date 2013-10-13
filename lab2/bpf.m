c5c42d404d53d5604834f4c5383924557975c357
afrolovskiy <frolovskiyav@gmail.com>
2013-10-14 00:40:53 +0400

    made corrections

--- lab2/bpf.m
+++ lab2/bpf.m
@@ -2,7 +2,6 @@ function [v] = bpf(u)
     N = length(u);
     v = zeros(1, N);
 
-    s = @(k)(u(k)*(-1)^(k-1));
     N1 = floor(N/2);    
     koeff = -2*pi*1i/N;
     for k=1:N1
@@ -10,8 +9,8 @@ function [v] = bpf(u)
         
         for n=1:N1
             W = exp(2*koeff*(k-1)*(n-1));
-            S0 = S0 + s(2*n-1) * W; 
-            S1 = S1 + s(2*n) * W; 
+            S0 = S0 + u(2*n-1) * W; 
+            S1 = S1 + u(2*n) * W * (-1); 
         end
    
         W = exp(koeff*(k-1));
