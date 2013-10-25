1bad253190d658f532abf9ab674c3a475c39eef3
afrolovskiy <frolovskiyav@gmail.com>
2013-10-25 14:40:17 +0400

    made corrections

--- lab3/draw_result.m
+++ lab3/draw_result.m
@@ -1,4 +1,4 @@
-function [] = draw_result(u, h, x_start, x_end, dx)
+function [] = draw_result(u, h, x_start, x_end, dx, uname, hname)
     [u_disc, x] = discretize(u, x_start, x_end, dx);
     [h_disc, x] = discretize(h, x_start, x_end, dx);
     [u_output] = convolution(u_disc, h_disc);
@@ -6,8 +6,14 @@ function [] = draw_result(u, h, x_start, x_end, dx)
     figure;
     hold on;
     
+    subplot(3, 1, 1)
     plot(x, u_disc, 'k', 'LineWidth', 3);
+    title(uname);
+    
+    subplot(3, 1, 2)
     plot(x, h_disc, 'r', 'LineWidth', 2);
+    title(hname);
+    
     
     N = length(u_output);
     x = zeros(1, N);
@@ -15,7 +21,8 @@ function [] = draw_result(u, h, x_start, x_end, dx)
     for i=2:N
         x(i) = x(i-1) + dx;
     end
-    figure;
+    subplot(3, 1, 3)
     plot(x, u_output, 'b');
+    title(['convolution of ', uname, ' & ', hname]);
 end
 
--- lab3/lab3.m
+++ lab3/lab3.m
@@ -9,6 +9,6 @@ a = -3.0;
 b = 3.0;
 dx = 0.1;
 
-%draw_result(@impulse, @impulse, a, b, dx);
-draw_result(@impulse, @gauss, a, b, dx);
-%draw_result(@gauss, @gauss, a, b, dx);
+draw_result(@impulse, @impulse, a, b, dx, 'impulse', 'impulse');
+draw_result(@impulse, @gauss, a, b, dx, 'impulse', 'gauss');
+draw_result(@gauss, @gauss, a, b, dx, 'gauss', 'gauss');
